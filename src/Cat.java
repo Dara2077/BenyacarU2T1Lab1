83fd5b90f450f64f23adc7a024c275b3d9cfb3c0
darabenyacar <dbenyacar1@gmail.com>
2022-09-29 20:10:31 -0400

    Lab 1 Cat

--- src/Cat.java
+++ src/Cat.java
@@ -28,6 +28,7 @@ public class Cat {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Weight: " + weight);
+        System.out.println("Test");
     }
 }
 
